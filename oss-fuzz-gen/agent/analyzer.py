545eaeb2ee8a074eb54bd327bd656bf47090fb90
ch1hyun <och2510@gmail.com>
2024-11-03 07:24:01 +0000

    f

--- oss-fuzz-gen/agent/analyzer.py
+++
@@ -1,6 +0,0 @@
-"""A base LLM agent to analyze the evaluation result of a fuzz target."""
-from agent.base_agent import BaseAgent
-
-
-class Analyzer(BaseAgent):
-  pass
--- oss-fuzz-gen/tool/lldb_tool.py
+++
@@ -1,6 +0,0 @@
-"""A tool for LLM agents to interact within a LLDB."""
-from tool.base_tool import BaseTool
-
-
-class LLDBTool(BaseTool):
-  pass
